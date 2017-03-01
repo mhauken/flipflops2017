14d36de9d63206547f0800a6b3039c5f23aed1d8
Marius Hauken <marius.hauken@gmail.com>
2017-03-01 17:01:37 +0000

    removed unused action

--- src/actions.js
+++ src/actions.js
@@ -2,11 +2,6 @@ export const pickTime = (timeDuration) => {
   return { type: 'PICK_TIME', timeDuration };
 };
 
-export const openLocationDetail = (locationId) => {
-  return { type: 'OPEN_LOCATION', locationId };
-}
-
 export const addComment = (username, comment) => {
   return { type: 'ADD_COMMENT', username, comment};
 }
-
